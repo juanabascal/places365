ad58040f779ab2aea2480e8d818096d49d3e73b0
Juan <juanabascalsanchez@gmail.com>
2018-02-28 15:10:07 +0100

    Predict working accurately

--- run.py
+++ run.py
@@ -1,29 +1,30 @@
 import tensorflow as tf
-import numpy as np
-from PIL import Image
 import preprocessing
 
-from model.google_net import GoogleNet
 from model.vgg_16 import VGG16
 
 input_width = 224
 input_height = 224
 num_channels = 3
 
-image_path = "./data/castle.jpg"
+image_path = "./data/stadium.jpg"
 
-# Load the input
-resized_image = preprocessing.image_resizing(image_path, input_width, input_height)
-np_image = preprocessing.from_image_to_np(resized_image).reshape(1, input_width, input_height, num_channels)
 
 def main(unused_arguments):
+    # Load the input
+    resized_image = preprocessing.image_resizing(image_path, input_width, input_height)
+    np_image = preprocessing.from_image_to_np(resized_image).reshape(1, input_width, input_height, num_channels)
+
     input_data = tf.placeholder(tf.float32, shape=[None, input_width, input_height, 3])
 
+    # Load the net
     net = VGG16({'data': input_data})
 
-    sess = tf.Session()
-    net.load('data/vgg16_places365.npy', sess)
-    sess.run(net.get_output(), feed_dict={input_data: np_image})
+    with tf.Session() as sess:
+        net.load('data/vgg16_places365.npy', sess)
+        predictions = sess.run(net.get_output(), feed_dict={input_data: np_image})
+
+        print"{} - {}".format(predictions.argmax(), predictions[0, predictions.argmax()])
 
 
 if __name__ == "__main__":
