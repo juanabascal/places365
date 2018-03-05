c17d051670ba37e36ec9229080eeee7e682ab1c1
Juan <juanabascalsanchez@gmail.com>
2018-03-05 11:26:42 +0100

    Prettier result printing

--- run.py
+++ run.py
@@ -2,24 +2,18 @@ import tensorflow as tf
 import preprocessing
 import numpy as np
 
-import sys
-
 import model.helper as model_helper
 
 image_path = "./data/castle.jpg"
-weights_path = "./data/vgg16_places365.npy"
+weights_path = "./data/googlenet_places365.npy"
 labels_path = "./data/labels.txt"
 
 
 def main(unused_arguments):
-    helper = model_helper.Helper('vgg16')
+    helper = model_helper.Helper('googlenet')
 
     # Load the input
-    resized_image = preprocessing.image_resizing(image_path, helper.net.input_width, helper.net.input_height)
-    np_image = preprocessing.from_image_to_np(resized_image).reshape(1,
-                                                                     helper.net.input_width,
-                                                                     helper.net.input_width,
-                                                                     helper.net.num_channels)
+    np_image = load_the_image(image_path, helper)
 
     input_data = tf.placeholder(tf.float32, shape=[None,
                                                    helper.net.input_width,
@@ -34,6 +28,14 @@ def main(unused_arguments):
         print_results(predictions)
 
 
+def load_the_image(image_path, helper):
+    resized_image = preprocessing.image_resizing(image_path, helper.net.input_width, helper.net.input_height)
+    return preprocessing.from_image_to_np(resized_image).reshape(1,
+                                                                 helper.net.input_width,
+                                                                 helper.net.input_width,
+                                                                 helper.net.num_channels)
+
+
 def print_results(predictions):
     # Convert predictions to numpy
     np_pred = np.array(predictions).flatten()
@@ -45,7 +47,7 @@ def print_results(predictions):
     labels = open(labels_path, "r").readlines()
 
     for i in range(0, 5):
-        print"Es un {} con probabilidad de {}".format(labels[np_index[i]], np_pred[np_index[i]])
+        print"Es un {} con probabilidad de {}".format(labels[np_index[i]].rstrip('\n'), np_pred[np_index[i]])
         print"---------------"
 
 
